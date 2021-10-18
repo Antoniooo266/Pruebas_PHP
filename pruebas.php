e0d50af144a2c8791f35260573fcdba270bbd48e
Antoniooo266 <acrocamora@gmail.com>
2021-10-18 09:09:53 +0200

    hiaewdad

---
+++ pruebas.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <title>Pruebas PHP </title>
+</head>
+<body>
+<h1>Tabla del 1 </h1>
+<table border="1">
+<?php
+$i=1;
+$n =1;
+
+while ($i<=10) {
+    echo"<tr><td>";
+    echo   $n ."<td>". "x"."<td>".$i ."<td>" ."="."<td>".$n*$i."</td></tr>";
+    $i++;
+}
+?>
+</table>
+<h1>Ejemplo 1</h1>
+<?php
+printf("El numero dos con diferenes formatos: %d %f %.2f",2,2,2);
+?>
+<h1>Cadena</h1>
+<?php
+$cadena="Esta cadena tiene 31 caracteres";
+echo strlen($cadena);
+echo"<br>";
+$resultado=sprintf("8*5 = %d <br>", 8*5);
+echo $resultado,"<br>";
+echo substr("Devuelve una subcadena de otra", 9,3),"<br><br>";
+if(chop("Cadena \n\n")=="Cadena")
+    echo "Iguales<br><br>";
+echo str_replace("azul","verde","Atrapado en tu mirada azul,tan azul como en mismo sueños.."),"<br>";
+?>
+</body>
+</html>
